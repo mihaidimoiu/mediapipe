44ac2fabb6641cc7221acdc081a65106a6f2fc74
Mihai Dimoiu <mihaidimoiu@yahoo.com>
2025-12-03 21:25:19 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
         .binaryTarget(
             name: "MediaPipeTasksCommon",
             url: "https://github.com/mihaidimoiu/mediapipe/releases/download/v0.10.26/MediaPipeTasksCommon.xcframework.zip",
-            checksum: "6860b50500d71121f30b19023fdbae3359cccd1fda8afdfa019c0bdceb39a37f"
+            checksum: "75fa0adb736145dfeaed5e9b6b36886f756263c8f23474e4ad6cafa267c4ccac"
         ),
 
         // Wrapper target for MediaPipeTasksCommon to add system framework dependencies
@@ -56,7 +56,7 @@ let package = Package(
         .binaryTarget(
             name: "MediaPipeTasksVision",
             url: "https://github.com/mihaidimoiu/mediapipe/releases/download/v0.10.26/MediaPipeTasksVision.xcframework.zip",
-            checksum: "16f542b02a36b383b6c043c5efc8cbfc177102d8f568421fa4f010dbb6821e76"
+            checksum: "32f85e7ff349bee1faefcc0a4c4f703377fc6e73a45d4493fdee3e068e0932e3"
         ),
 
         // MediaPipeTasksText - Text task APIs
@@ -64,7 +64,7 @@ let package = Package(
         .binaryTarget(
             name: "MediaPipeTasksText",
             url: "https://github.com/mihaidimoiu/mediapipe/releases/download/v0.10.26/MediaPipeTasksText.xcframework.zip",
-            checksum: "98b5873f24cb43bea72cceadf29449de6211f43fd74ab808cab8a42d6f28bea1"
+            checksum: "4a7d672c58873d0d13f8bacf766b51cfc89adf90a5efbb57dade33b0c51f4fa1"
         ),
 
         // MediaPipeTasksAudio - Audio task APIs
@@ -72,7 +72,7 @@ let package = Package(
         .binaryTarget(
             name: "MediaPipeTasksAudio",
             url: "https://github.com/mihaidimoiu/mediapipe/releases/download/v0.10.26/MediaPipeTasksAudio.xcframework.zip",
-            checksum: "265069d950a033b82882f434dbd3062a296419b616f1ed11c72ec4c2c10705b6"
+            checksum: "458f4865afd5e7690f6a0d8b2eddb58c4a9c565c9e04edee463c9180fbd03a52"
         ),
     ]
 )
